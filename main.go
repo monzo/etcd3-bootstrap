5add250f4b644d901f55f11b17aecafb957167ac
Filipe Varela <filipe@monzo.com>
2017-10-09 10:46:34 +0100

    Default to /var/lib/etcd mount point

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ var (
 func init() {
 	flag.StringVar(&awsRegion, "aws-region", "eu-west-1", "AWS region this instance is on")
 	flag.StringVar(&ebsVolumeName, "ebs-volume-name", "", "EBS volume to attach to this node")
-	flag.StringVar(&mountPoint, "mount-point", "/var/lib/etcd-bootstrap", "EBS volume mount point")
+	flag.StringVar(&mountPoint, "mount-point", "/var/lib/etcd", "EBS volume mount point")
 	flag.StringVar(&blockDevice, "block-device", "/dev/xvdf", "Block device to attach as")
 	flag.Parse()
 }
